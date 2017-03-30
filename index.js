1c7de6df912176bb5c87c62722fb296def6f06d4
osk2 <sc60714@gmail.com>
2017-03-30 17:31:14 +0800

    Clean-up code

--- index.js
+++ index.js
@@ -54,11 +54,6 @@ module.exports = function(html, options) {
     $('head').append('<style amp-boilerplate="">body{-webkit-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-moz-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-ms-animation:-amp-start 8s steps(1,end) 0s 1 normal both;animation:-amp-start 8s steps(1,end) 0s 1 normal both}@-webkit-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-moz-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-ms-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-o-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}</style><noscript><style amp-boilerplate="">body{-webkit-animation:none;-moz-animation:none;-ms-animation:none;animation:none}</style></noscript>');
   }
 
-  /* main amp library */
-  if ($('head script[src="https://cdn.ampproject.org/v0.js"]').length === 0) {
-    $('head').append('<script async src="https://cdn.ampproject.org/v0.js"></script>');
-  }
-
   /* img dimensions */
   $('img:not(width):not(height)').each(function() {
     var src = $(this).attr('src');
