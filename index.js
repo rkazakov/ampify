66868330de150c15c99a0ae343fe3d0dd206af92
Ruslan Kazakov <ruslan.kazakov@gmail.com>
2018-05-27 22:42:28 +1000

    Refactor library code and apply linting

--- index.js
+++ index.js
@@ -3,35 +3,32 @@ const url = require('url');
 const cheerio = require('cheerio');
 const request = require('request');
 const sizeOf = require('image-size');
-const cleanCss = require('clean-css');
+const CleanCss = require('clean-css');
 
-module.exports = function (html, options) {
-  var tags = {
-    amp: ['img', 'video']
+module.exports = (html, options) => {
+  const tags = {
+    amp: ['img', 'video'],
   };
 
-  var $, round, youtube = false;
-  var options = options || {};
+  let youtube = false;
 
-  options.normalizeWhitespace = options.normalizeWhitespace || false;
-  options.xmlMode = options.xmlMode || false;
-  options.decodeEntities = options.decodeEntities || false;
+  const cheerioOptions = options || {
+    cwd: options.cwd || '',
+    round: options.round || true,
+    normalizeWhitespace: options.normalizeWhitespace || false,
+    xmlMode: options.xmlMode || false,
+    decodeEntities: options.decodeEntities || false,
+  };
 
-  options.cwd = options.cwd || '';
-  options.round = options.round || true;
+  const $ = cheerio.load(html, cheerioOptions);
 
-  $ = cheerio.load(html, options);
-
-  if (options.round) {
-    round = function (numb) { return Math.round(numb / 5) * 5; }
-  }
-  else {
-    round = function (numb) { return numb; }
-  }
+  const round = cheerioOptions.round
+    ? numb => Math.round(numb / 5) * 5
+    : numb => numb;
 
   /* html ⚡ */
-  $('html').each(function () {
-    $(this).attr('amp', '');
+  $('html').each((index, element) => {
+    $(element).attr('amp', '');
   });
 
   /* head */
@@ -46,12 +43,12 @@ module.exports = function (html, options) {
   $('head').prepend('<meta charset="utf-8">');
 
   /* google analytics */
-  $('script').each(function() {
-    var src = $(this).attr('src');
+  $('script').each((index, element) => {
+    const src = $(element).attr('src');
     if (src) {
-      var trackingId = src.match(/\bUA-\d{4,10}-\d{1,4}\b/);
+      const trackingId = src.match(/\bUA-\d{4,10}-\d{1,4}\b/);
       if (trackingId) {
-        $(this).remove();
+        $(element).remove();
         $('head').prepend('<script async custom-element="amp-analytics"src="https://cdn.ampproject.org/v0/amp-analytics-0.1.js"></script>');
         $('body').append(`<amp-analytics type="googleanalytics">
           <script type="application/json">
@@ -69,9 +66,10 @@ module.exports = function (html, options) {
         </amp-analytics>`);
       }
     }
-    var scriptContent = $(this).html();
-    if (scriptContent && scriptContent.match(/function gtag\(\){dataLayer\.push\(arguments\);}/)) {
-      $(this).remove();
+    const scriptContent = $(element).html();
+    const htmlScriptContent = scriptContent.match(/function gtag\(\){dataLayer\.push\(arguments\);}/);
+    if (scriptContent && htmlScriptContent) {
+      $(element).remove();
     }
   });
 
@@ -88,66 +86,64 @@ module.exports = function (html, options) {
   /* body */
 
   /* img dimensions */
-  $('img:not(width):not(height)').each(function () {
-    var src = $(this).attr('src');
+  $('img:not(width):not(height)').each((index, element) => {
+    const src = $(element).attr('src');
     if (!src) {
-      return $(this).remove();
+      return $(element).remove();
     }
     if (src.indexOf('//') === -1) {
-      var image = options.cwd + '/' + $(this).attr('src');
+      const image = `${options.cwd}/${$(element).attr('src')}`;
       if (fs.existsSync(image)) {
-        var size = sizeOf(image);
-        $(this).attr({
+        const size = sizeOf(image);
+        $(element).attr({
           width: round(size.width),
-          height: round(size.height)
+          height: round(size.height),
+        });
+      }
+    } else if (src.indexOf('//') !== -1) {
+      const imageUrl = element.attribs.src;
+      const response = request('GET', imageUrl);
+      if (response.statusCode === 200) {
+        const size = sizeOf(response.body);
+        $(element).attr({
+          width: round(size.width),
+          height: round(size.height),
         });
       }
     }
-    else if (src.indexOf('//') != -1) {
-      var imageUrl = this.attribs.src;
-      var response = request('GET', imageUrl);
-      if (response.statusCode === 200) {
-        var size = sizeOf(response.body);
-        $(this).attr({
-          width: round(size.width),
-          height: round(size.height)
-        });
-      }
-    };
   });
 
   /* inline styles */
-  $('link[rel=stylesheet]').each(function () {
-    var src = $(this).attr('href');
-    var path = src;
-    var file = '';
-    var setFile = function (data) {
-      var minified = new cleanCss().minify(data).styles;
-      return '<style amp-custom>' + minified + '</style>';
+  $('link[rel=stylesheet]').each((index, element) => {
+    const src = $(element).attr('href');
+    let path = src;
+    let file = '';
+    const setFile = (data) => {
+      const minified = new CleanCss().minify(data).styles;
+      return `<style amp-custom>${minified}</style>`;
     };
 
-		try {
-			if (src.indexOf('//') === -1) {
-				path = options.cwd + '/' + src;
-				if (fs.existsSync(path)) {
-					file = setFile(String(fs.readFileSync(path)));
-				}
-			}
-			else if (src.indexOf('//') != -1) {
-				file = setFile(String(request('GET', path).body));
-			};
-		} catch (err) {
-			console.dir(err);
-		}
-    $(this).replaceWith(file);
+    try {
+      if (src.indexOf('//') === -1) {
+        path = `${options.cwd}/${src}`;
+        if (fs.existsSync(path)) {
+          file = setFile(String(fs.readFileSync(path)));
+        }
+      } else if (src.indexOf('//') !== -1) {
+        file = setFile(String(request('GET', path).body));
+      }
+    } catch (err) {
+      console.dir(err);
+    }
+    $(element).replaceWith(file);
   });
 
   /* youtube */
-  $('iframe[src*="http://www.youtube.com"]').each(function () {
+  $('iframe[src*="http://www.youtube.com"]').each((index, element) => {
     youtube = true;
-    const src = $(this).attr('src');
-    const width = $(this).attr('width');
-    const height = $(this).attr('height');
+    const src = $(element).attr('src');
+    const width = $(element).attr('width');
+    const height = $(element).attr('height');
     const path = url.parse(src).pathname.split('/');
     const ampYoutube = `
     <amp-youtube
@@ -156,7 +152,7 @@ module.exports = function (html, options) {
       height="${height}"
       layout="responsive">
     </amp-youtube>`;
-    $(this).replaceWith(ampYoutube);
+    $(element).replaceWith(ampYoutube);
   });
 
   if (youtube) {
@@ -164,9 +160,12 @@ module.exports = function (html, options) {
   }
 
   /* amp tags */
-  $(tags.amp.join(',')).each(function () {
-    this.name = 'amp-' + this.name;
+  $(tags.amp.join(',')).each((index, element) => {
+    const ampElement = Object.assign(element, {
+      name: `amp-${element.name}`,
+    });
+    $(element).replaceWith(ampElement);
   });
 
   return $.html();
-}
+};
